5742e96fda9fbf64493651855a6177ee082d9281
Kirill Lopatin <skwize.work@gmail.com>
2026-01-05 12:41:08 +0300

    Added features animation scrolling

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,6 @@
 'use strict'
-const system_mon = document.getElementById("system_monitoring");
-const window_man = document.getElementById("window_manager");
-const keyboard_clean = document.getElementById("keyboard_cleaning");
-const system_monitoring_page = document.getElementById("system_monitoring_page");
-let cpuData = 0;
-let ramData = 0;
-let gpuData = 0;
-let cpuAnimation;
-let ramAnimation;
-let gpuAnimation;
-let cpuMouseOver;
-let ramMouseOver;
-let gpuMouseOver;
-let isAnimStarted = false;
-
 window.onload = function () {
-    startAnimation();
+    // Set event listeners
     document.getElementById("cpu-graph").addEventListener('mouseenter', ()=>{
         document.getElementById('cpu-main-text').style.opacity = 0;
         document.getElementById("cpu-percentage").style.opacity = 100;
@@ -40,95 +25,184 @@ window.onload = function () {
         document.getElementById('gpu-main-text').style.opacity = 100;
         document.getElementById("gpu-percentage").style.opacity = 0;
     });
+
+    //Start Animations
+    startFeaturePagesAnimation();
 }
 
+const sys_mon_span      = document.getElementById("feature_sys");
+const win_man_span      = document.getElementById("feature_win");
+const kb_clean_span     = document.getElementById("feature_key");
+
+let cpuData;
+let ramData;
+let gpuData;
+
+const systemMonitoringPage  = document.getElementById("system_monitoring_page");
+const windowManagerPage     = document.getElementById("window_manager_page");
+const keyboardCleaningPage  = document.getElementById("keyboard_cleaning_page");
+
+let page_number = 0; // 0 sys 1 win 2 key
+
+let feature_pages_animation             = undefined;
+let system_monitoring_page_animation    = undefined;
+let window_manager_page_animation       = undefined; // Not implemented
+let live_wallpaper_page_animation       = undefined; // Not implemented
+let re_started_animation                = undefined;
+
+
+// System monitoring graphs animation
 function getFakeData (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function startSysMonitoringAnimation () {
 
+    if (system_monitoring_page_animation !== undefined) {
+        return;
+    }
 
-function startAnimation () {
-    cpuAnimation = setInterval(() => {
+    system_monitoring_page_animation = setInterval(() => {
+        //CPU graph animation
         cpuData = getFakeData(15, 25);
         document.getElementById("cpu-percentage").textContent = cpuData + "%"
         document.getElementById("progress_cpu").style.strokeDashoffset = 565.48 * (100-cpuData) / 100;
-    }, 1500);
-    ramAnimation = setInterval(() => {
+        //RAM graph animation
         ramData = getFakeData(30, 45);
         document.getElementById("ram-percentage").textContent = ramData + "%"
         document.getElementById("progress_ram").style.strokeDashoffset = 565.48 * (100 - ramData) / 100;
-    }, 1500);
-    gpuAnimation = setInterval(() => {
+        //GPU graph animation
         gpuData = getFakeData(5, 10);
         document.getElementById("gpu-percentage").textContent = gpuData + "%"
         document.getElementById("progress_gpu").style.strokeDashoffset = 565.48 * (100 - gpuData) / 100;
-    }, 1500);
-    isAnimStarted = true;
+    }, 2000);
 }
 
-
-
-function replaceRamData () {
-    document.getElementById("cpu-percentage").innerText = cpuData;
+function stopSysMonitoringAnimation () {
+    clearInterval(system_monitoring_page_animation);
+    system_monitoring_page_animation = undefined;
 }
 
-function replaceGpuData () {
-    document.getElementById("cpu-percentage").innerText = cpuData;
-}
+// Animation of page changing
+function startFeaturePagesAnimation () {
+    if (feature_pages_animation !== undefined) {
+        return;
+    }
 
-function stopAnimation () {
-    clearInterval(cpuAnimation);
-    clearInterval(ramAnimation);
-    clearInterval(gpuAnimation);
-    isAnimStarted = false;
-}
+    systemMonitoringPage.style.opacity = 100;
+    startSysMonitoringAnimation();
 
-const select_page_first = () => {
-    if (system_mon.checked == true)
-    {
-        if (!isAnimStarted){
-            startAnimation();
+    feature_pages_animation = setInterval (() => {
+        switch (page_number){
+            case 0:
+                
+                sys_mon_span.style.color = "#3e3e3e";
+                win_man_span.style.color = "#bfbfbf";
+                kb_clean_span.style.color = "#bfbfbf";
+                
+                startSysMonitoringAnimation();
+                
+                systemMonitoringPage.style.opacity = 100;
+                windowManagerPage.style.opacity = 0;
+                keyboardCleaningPage.style.opacity = 0;
+                page_number = 1;
+                break;
+
+            case 1:
+                
+                sys_mon_span.style.color = "#bfbfbf";
+                win_man_span.style.color = "#3e3e3e";
+                kb_clean_span.style.color = "#bfbfbf";
+                
+                stopSysMonitoringAnimation();
+                
+                systemMonitoringPage.style.opacity = 0;
+                windowManagerPage.style.opacity = 100;
+                keyboardCleaningPage.style.opacity = 0;
+
+                page_number = 2;
+                break;
+            
+            case 2:
+                
+                stopSysMonitoringAnimation();
+                sys_mon_span.style.color = "#bfbfbf";
+                win_man_span.style.color = "#bfbfbf";
+                kb_clean_span.style.color = "#3e3e3e";
+                
+                systemMonitoringPage.style.opacity = 0;
+                windowManagerPage.style.opacity = 0;
+                keyboardCleaningPage.style.opacity = 100;
+
+                page_number = 0;
+                break;
         }
-        document.getElementById("system_monitoring_page").style.opacity = 100;
-        document.getElementById("feature_sys").style.color = "#3E3E3E";
-        document.getElementById("feature_win").style.color = "#BFBFBF";
-        document.getElementById("feature_key").style.color = "#BFBFBF";
-    }else{
-        system_mon.checked = true;
-        document.getElementById("system_monitoring_page").style.opacity = 100;
-        document.getElementById("feature_sys").style.color = "#3E3E3E";
-        document.getElementById("feature_win").style.color = "#BFBFBF";
-        document.getElementById("feature_key").style.color = "#BFBFBF";
-    }
+
+    }, 6500)
 }
-const select_page_sec = () => {
-    if (window_man.checked)
-    {
-        stopAnimation();
-        document.getElementById("system_monitoring_page").style.opacity = 0;
-        document.getElementById("feature_sys").style.color = "#BFBFBF";
-        document.getElementById("feature_win").style.color = "#3E3E3E";
-        document.getElementById("feature_key").style.color = "#BFBFBF";
-    }else{
-        window_man.checked = true;
-        stopAnimation();
-        document.getElementById("system_monitoring_page").style.opacity = 0;
-        document.getElementById("feature_sys").style.color = "#BFBFBF";
-        document.getElementById("feature_win").style.color = "#3E3E3E";
-        document.getElementById("feature_key").style.color = "#BFBFBF";
-    }
+
+function stopFeaturePagesAnimation () {
+    clearInterval(feature_pages_animation);
+    feature_pages_animation = undefined;
 }
-const select_page_third = () => {
-    if (keyboard_clean.checked)
-    {
-        document.getElementById("feature_sys").style.color = "#BFBFBF";
-        document.getElementById("feature_win").style.color = "#BFBFBF";
-        document.getElementById("feature_key").style.color = "#3E3E3E";
-    }else{
-        keyboard_clean.checked = true;
-        document.getElementById("feature_sys").style.color = "#BFBFBF";
-        document.getElementById("feature_win").style.color = "#BFBFBF";
-        document.getElementById("feature_key").style.color = "#3E3E3E";
+
+function re_startFeaturePagesAnimation() {
+    if (re_started_animation !== undefined){
+        clearTimeout(re_started_animation);
     }
+
+    re_started_animation = setTimeout(()=>{
+        startFeaturePagesAnimation();
+    },4500);
+}
+
+
+// Page navigation
+function select_page_first () {
+    sys_mon_span.style.color = "#3e3e3e";
+    win_man_span.style.color = "#bfbfbf";
+    kb_clean_span.style.color = "#bfbfbf";
+
+    stopFeaturePagesAnimation();
+    startSysMonitoringAnimation();
+    
+    systemMonitoringPage.style.opacity = 100;
+    windowManagerPage.style.opacity = 0;
+    keyboardCleaningPage.style.opacity = 0;
+
+    page_number = 0;
+    re_startFeaturePagesAnimation();
+    return;
+
+}
+function select_page_sec () {
+    sys_mon_span.style.color = "#bfbfbf";
+    win_man_span.style.color = "#3e3e3e";
+    kb_clean_span.style.color = "#bfbfbf";
+    
+    stopSysMonitoringAnimation();
+    
+    systemMonitoringPage.style.opacity = 0;
+    windowManagerPage.style.opacity = 100;
+    keyboardCleaningPage.style.opacity = 0;
+
+    page_number = 1;
+    re_startFeaturePagesAnimation();
+
+    return;
+}
+function select_page_third () {
+    stopSysMonitoringAnimation();
+    
+    sys_mon_span.style.color = "#bfbfbf";
+    win_man_span.style.color = "#bfbfbf";
+    kb_clean_span.style.color = "#3e3e3e";
+    
+    systemMonitoringPage.style.opacity = 0;
+    windowManagerPage.style.opacity = 0;
+    keyboardCleaningPage.style.opacity = 100;
+
+    page_number = 2;
+    re_startFeaturePagesAnimation();
+    return;
 }
